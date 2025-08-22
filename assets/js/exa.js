a8a3331c368701cd92165e0e0a906795f56995cd
Benjamin Cassiday <BenCassiday@gmail.com>
2025-08-22 16:09:31 -0700

    removed images tab

--- assets/js/exa.js
+++ assets/js/exa.js
@@ -154,14 +154,7 @@ jQuery(document).ready(function($) {
                         </svg>
                         Answer
                     </button>
-                    <button class="tab-btn" data-tab="images">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                            <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                            <circle cx="8.5" cy="8.5" r="1.5"></circle>
-                            <polyline points="21,15 16,10 5,21"></polyline>
-                        </svg>
-                        Images
-                    </button>
+
                     <button class="tab-btn" data-tab="sources">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <polygon points="12,2 15.09,8.26 22,9.27 17,14.14 18.18,21.02 12,17.77 5.82,21.02 7,14.14 2,9.27 8.91,8.26"></polygon>
